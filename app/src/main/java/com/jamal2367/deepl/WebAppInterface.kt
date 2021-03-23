6195cff3d94821591cabc69bc81d751621338452
Jamal <jamal2362@googlemail.com>
2021-03-23 21:16:11 +0100

    So much changes to write it here.

---
+++ app/src/main/java/com/jamal2367/deepl/WebAppInterface.kt
@@ -0,0 +1,18 @@
+package com.jamal2367.deepl
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.webkit.JavascriptInterface
+import android.widget.Toast
+
+class WebAppInterface(private val context: Context) {
+
+    @JavascriptInterface
+    fun copyClipboard(text: String) {
+        val clipboard: ClipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip: ClipData = ClipData.newPlainText("translation_text", text)
+        clipboard.setPrimaryClip(clip)
+        Toast.makeText(context, context.getString(R.string.copy_clipboard), Toast.LENGTH_SHORT).show()
+    }
+}
