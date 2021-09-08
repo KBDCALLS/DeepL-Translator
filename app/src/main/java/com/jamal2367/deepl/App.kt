0b6a96835159c00d78b83d800179ad01952ff367
Jamal <jamal2362@googlemail.com>
2021-09-08 11:04:10 +0200

    Restore FloatingTextSelection Activity

--- app/src/main/java/com/jamal2367/deepl/App.kt
+++ app/src/main/java/com/jamal2367/deepl/App.kt
@@ -19,25 +19,19 @@ class App : Application() {
 
     private fun switchTheme() {
         val config = getSharedPreferences("config", Context.MODE_PRIVATE)
-        var darkThemeMode =
-            config.getString(
-                getString(R.string.key_dark_mode),
-                AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM.toString()
-            )!!
+        var darkThemeMode = config.getString(getString(R.string.key_dark_mode), AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM.toString())!!
 
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q && darkThemeMode == AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM.toString()) {
             darkThemeMode = AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY.toString()
         }
+
         AppCompatDelegate.setDefaultNightMode(darkThemeMode.toInt())
     }
 
     @RequiresApi(Build.VERSION_CODES.N_MR1)
     private fun removeOldAppShortcut() {
         val shortcutManager = getSystemService(ShortcutManager::class.java)
-        shortcutManager.removeDynamicShortcuts(
-            listOf(
-                "theme"//version 5.0(17) Used to launch this app with darkTheme or lightTheme.
-            )
-        )
+        // Used to launch this app with darkTheme or lightTheme.
+        shortcutManager.removeDynamicShortcuts(listOf("theme"))
     }
 }
